658e4fb5abff7a9be5d7950e7845639e07780eb3
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2020-02-27 08:54:26 -0800

    all tests for different frameworks.

--- src/first_duplicate01/main.rs
+++ src/first_duplicate01/main.rs
@@ -10,26 +10,35 @@ find first duplicate element on given list.
 */
 
 fn find_duplicate(es: Vec<&str>) -> &str {
-    let mut seen: HashMap<&str, usize> = HashMap::new();
-    let mut first_dup_index: usize = es.len();
-    let mut first_dup: &str = "";
+    // Count all elements and where they appear first.
+    //
+    // element -> (index, count)
+    let mut seen: HashMap<&str, (usize, usize)> = HashMap::new();
     for (i, e) in es.iter().enumerate() {
-        if seen.contains_key(e) && seen.get(e).unwrap() < &first_dup_index {
-            first_dup_index = *seen.get(e).unwrap();
-            first_dup = e;
+        if seen.contains_key(e) {
+            let (index, count) = *seen.get(e).unwrap();
+            seen.insert(e, (index, count + 1));
         } else {
-            seen.insert(e, i);
+            seen.insert(e, (i, 1));
         }
-
-
         info!("{}: {}", e, i);
     }
-    return first_dup;
+
+
+    let mut els: Vec<(usize, usize, &str)> = seen.drain()
+        .map(|(e, (index, count))| (index, count, e))
+        .filter(|(_, count, _)| count > &1).collect();
+
+    // This call makes the algorithm nlog(n). But it is possible to do this part
+    // in O(n) time complexity
+    els.sort();
+
+    let (_, _, el) = els[0];
+    el
 }
 
 fn main() {
     pretty_env_logger::init();
-    debug!("Hello, world!");
-    find_duplicate(vec!["a", "b", "c", "d", "c", "b"]);
-
+    let resp = find_duplicate(vec!["a", "b", "c", "d", "c", "b"]);
+    debug!("First Duplicate: {}", resp);
 }
